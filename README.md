54aaf6373f287ff0ffe038ec03df284665103bc2
aliaz001 <alihosene00093@gmail.com>
2017-07-18 23:18:01 +0430

    sam
    sam

---
+++ README.md
@@ -0,0 +1,27 @@
+# tele_bom_bang_new (or) ENERGY | v11 (or) edit 11
+ستاره یادت نره حمایت کن دمت گرم
+
+cd $HOME && git clone https://github.com/telebombang2018/energy.git && cd energy && chmod +x energy.sh && ./energy.sh install && ./energy.sh
+________
+*****************
+نصب اتولانچ
+--------------
+cd energy 
+
+chmod +x autoenergy.sh 
+
+screen ./autoenergy.sh
+*****************
+لانچ دوباره
+--------------
+killall screen
+
+cd energy && chmod +x autoenergy.sh && screen ./autoenergy.sh
+
+*****************
+--------------
+کانال ما برای دریافت اپدیت ها و دریافت هلپر
+
+@energy_team
+
+@richenergy
---
+++ data/config.lua
@@ -0,0 +1,37 @@
+do local _ = {
+  admins = {},
+  disabled_channels = {},
+  enabled_plugins = {
+    "banhammer_fa",
+    "banhammer",
+    "gpsettings_en",
+    "gpsettings_fa",
+    "helpenergy",
+    "expire",
+    "nerkh",
+    "azan",
+    "del",
+    "id",
+    "mute-time",
+    "cleanbot",
+    "CleanMember",
+    "lock_fosh",
+    "monshi",
+    "msg-checks",
+    "plugins",
+    "tools",
+    "Invite",
+    "addkick",
+    "delall"
+  },
+  info_text = "》Beyond Reborn v6.0\nAn advanced administration bot based on https://valtman.name/telegram-cli\n\n》https://github.com/telebombang2018/tele_bom_bang_new \n\n\n\n》Special thanks to :\n》beyond_permag_bombang\n\n》Our channel :\n》@RICHENERGY%%@energy_team\n\n》Our website :\n》http://telebombang.blogfa.com\n",
+  moderation = {
+    data = "./data/moderation.json"
+  },
+  sudo_users = {
+    323046540,
+    323046540
+  }
+}
+return _
+end
